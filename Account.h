85bfa9f757f5d95b2ee9b170a4424e5c34e6e7eb
LuanTang <luantang.work@gmail.com>
2022-02-12 12:53:43 +0700

    + init project.

---
+++ Account.h
@@ -0,0 +1,14 @@
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+#include <string>
+#include <vector>
+
+class Account {
+public:
+    std::string user_name;
+    std::string password;
+    std::vector <int> user_vec;
+};
+
+#endif
+
